cd8143465062b660bc65f7a8039eb9943ef60cb6
dimitrmo <dmouratidis153@gmail.com>
2023-02-01 20:17:11 +0200

    Move channel

---
+++ src/channel.rs
@@ -0,0 +1,55 @@
+use serde::de::{Error, Unexpected, Visitor};
+use serde::{Deserialize, Deserializer};
+use std::fmt::Formatter;
+
+#[derive(Deserialize, Clone, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct Channel {
+    pub name: String,
+    pub size: usize,
+}
+
+#[derive(Default, Clone, Debug)]
+pub struct ChannelList {
+    pub channels: Vec<Channel>,
+}
+
+pub struct ChannelListDeserializer;
+
+impl ChannelList {
+    pub fn is_empty(&self) -> bool { self.channels.len() == 0 }
+    pub fn len(&self) -> usize {
+        self.channels.len()
+    }
+}
+
+impl<'de> Visitor<'de> for ChannelListDeserializer {
+    type Value = ChannelList;
+
+    fn expecting(&self, formatter: &mut Formatter) -> std::fmt::Result {
+        formatter.write_str("a valid json string representing an array of channels")
+    }
+
+    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
+        where
+            E: Error,
+    {
+        let data: Result<Vec<Channel>, serde_json::Error> = serde_json::from_str(v);
+        match data {
+            Err(err) => Err(Error::invalid_type(
+                Unexpected::Str(err.to_string().as_str()),
+                &self,
+            )),
+            Ok(data) => Ok(ChannelList { channels: data }),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for ChannelList {
+    fn deserialize<D>(deserializer: D) -> Result<ChannelList, D::Error>
+        where
+            D: Deserializer<'de>,
+    {
+        deserializer.deserialize_str(ChannelListDeserializer)
+    }
+}
