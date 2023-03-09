652b276385d1912e1c0a1c7c9e38143fc0f76644
dimitrmo <dmouratidis153@gmail.com>
2023-03-09 21:10:43 +0200

    Add helper function

--- src/message.rs
+++ src/message.rs
@@ -1,3 +1,4 @@
+use std::str;
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 
@@ -15,6 +16,14 @@ impl MessageValue {
             MessageValue::Text(text) => serde_json::from_str::<T>(text.as_str()),
             MessageValue::Json(value) => serde_json::from_value::<T>(value.clone()),
             MessageValue::Binary(data) => serde_json::from_slice::<T>(data),
+        }
+    }
+
+    pub fn as_str(&self) -> &str {
+        match self {
+            MessageValue::Text(text) => text.as_str(),
+            MessageValue::Json(value) => value.as_str().unwrap_or(""),
+            MessageValue::Binary(data) => str::from_utf8(data.as_slice()).unwrap_or("")
         }
     }
 }
