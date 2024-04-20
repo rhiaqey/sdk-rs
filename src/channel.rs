46a7dedb91d705143fd0b2434abca804dfab1c99
dimitrmo <dmouratidis153@gmail.com>
2024-04-20 11:13:08 +0300

    Introduce simple channel which holds channel name but can parse category

--- src/channel.rs
+++ src/channel.rs
@@ -1,4 +1,54 @@
 use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter};
+
+#[derive(Serialize, Deserialize, Clone, Debug, Eq, PartialEq)]
+#[serde(rename_all = "PascalCase")]
+pub struct SimpleChannel(String);
+
+impl SimpleChannel {
+    pub fn get_channel_with_category(&self) -> (String, Option<String>) {
+        let parts: Vec<&str> = self.0.split('/').collect();
+        match parts.len() {
+            2 => (parts[0].to_string(), Some(parts[1].to_string())),
+            _ => (parts[0].to_string(), None),
+        }
+    }
+}
+
+impl From<&str> for SimpleChannel {
+    fn from(value: &str) -> Self {
+        Self {
+            0: value.to_string(),
+        }
+    }
+}
+
+impl Display for SimpleChannel {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[serde(rename_all = "PascalCase")]
+pub struct SimpleChannels(Vec<SimpleChannel>);
+
+impl SimpleChannels {
+    pub fn get_channels_with_category(&self) -> Vec<(String, Option<String>)> {
+        self.0
+            .iter()
+            .map(|x| x.get_channel_with_category())
+            .collect()
+    }
+}
+
+impl From<Vec<&str>> for SimpleChannels {
+    fn from(value: Vec<&str>) -> Self {
+        Self {
+            0: value.iter().map(|x| SimpleChannel::from(*x)).collect(),
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 #[serde(rename_all = "PascalCase")]
