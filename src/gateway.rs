834289d56c017b8595430a23e1abee478ae8aebc
dimitrmo <dmouratidis153@gmail.com>
2024-07-09 22:45:41 +0300

    Add function to get id

--- src/gateway.rs
+++ src/gateway.rs
@@ -49,6 +49,15 @@ pub struct GatewayConfig {
     pub host: Option<String>,
 }
 
+impl GatewayConfig {
+    pub fn get_id(&self) -> &str {
+        match &self.id {
+            None => "",
+            Some(val) => val.as_str(),
+        }
+    }
+}
+
 impl Default for GatewayConfig {
     fn default() -> Self {
         GatewayConfig {
